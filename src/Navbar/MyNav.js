0161d13abce1cbf0f383c91214c60b3e871c5426
liutiki <liutiki@list.ru>
2024-07-25 13:52:41 +0300

    changed mynav

--- src/Navbar/MyNav.js
+++ src/Navbar/MyNav.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom'; // Импортируйте Link
 import './MyNav.css';
 
 function MyNav() {
   const [scrolled, setScrolled] = useState(false);
+  const { i18n, t } = useTranslation();
 
-  const {i18n, t}=useTranslation()
-
-  const toggleLang =() =>{
-      i18n.changeLanguage(i18n.language === 'en'? 'ru' : 'en')
-  }
-  
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,17 +31,20 @@ function MyNav() {
   return (
     <Navbar expand="lg" className={scrolled ? 'mynav scrolled fixed-top' : 'mynav fixed-top'}>
       <Container>
-      <Navbar.Brand > <button className="lanButton"onClick ={toggleLang}>{i18n.language === 'en' ? "RU" : "EN"}</button></Navbar.Brand>
-        <Navbar.Brand href="/">JK WEB</Navbar.Brand>
-       
+        <Navbar.Brand>
+          <button className="lanButton" onClick={toggleLang}>
+            {i18n.language === 'en' ? "RU" : "EN"}
+          </button>
+        </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">JK WEB</Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">{t('Home')}</Nav.Link>
-            <Nav.Link href="/AboutMe">{t('About')}</Nav.Link>
-            <Nav.Link href="/Projects">{t('Projects')}</Nav.Link>
-            <Nav.Link href="/Contacts">{t('Contacts')}</Nav.Link>
-            
+            <Nav.Link as={Link} to="/">{t('Home')}</Nav.Link> 
+            <Nav.Link as={Link} to="/AboutMe">{t('About')}</Nav.Link> 
+            <Nav.Link as={Link} to="/Projects">{t('Projects')}</Nav.Link> 
+            <Nav.Link as={Link} to="/Contacts">{t('Contacts')}</Nav.Link> 
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -51,4 +53,5 @@ function MyNav() {
 }
 
 export default MyNav;
+
 
