57686964cad2329014af9fb03d5fa6f3b5659e3f
liutiki <liutiki@list.ru>
2024-12-18 18:13:43 +0300

    wednesday commit december

--- src/MyRecentProjects/MyRecentProjects.js
+++ src/MyRecentProjects/MyRecentProjects.js
@@ -1,4 +1,5 @@
-import video from './video/video.mp4';
+import massage from './massage.jpg';
+import kmp from './kmp.png';
 import './Projects.css';
 import { useTranslation } from 'react-i18next';
 
@@ -8,20 +9,32 @@ function MyRecentProjects (){
 
 return(
     <div className="spaPosition">
-    <div className="videoPosition">
-                <video className="video"
-                src={video}
-                controls loop
-                >
-                </video>
-           
+<div className="comerse-position">
+<div className="massagePhotoPosition">
+              <img className="massageSalon" src={kmp} alt="kmp"/>           
     </div>
+<div className="masPosition">
+    <p className="Site">{t("Sea Shipping")}</p>
+    <button className="btn-spa"><a className="aSpa"href="https://kdsc.netlify.app" target="_blank" rel="noreferrer">{t('Watch the project')}</a></button>
+    <button className="btn-spa"><a className="aSpa"href="https://github.com/liutiki/KMP" target="_blank" rel="noreferrer">{t('Watch GitHub')}</a></button>
+</div>
+   
+</div>
 
+<div className="comerse-position">
+<div className="massagePhotoPosition">
+              <img className="massageSalon" src={massage} alt="massagesalon"/>           
+    </div>
+   
     <div className="masPosition">
-        <p className="My">{t('New site')}</p>
         <p className="Site">{t("Massage Spa Salon")}</p>
         <button className="btn-spa"><a className="aSpa"href="https://luminous-cheesecake-7bfe2a.netlify.app" target="_blank" rel="noreferrer">{t('Watch the project')}</a></button>
+        <button className="btn-spa"><a className="aSpa"href="https://github.com/liutiki/MassageSalon" target="_blank" rel="noreferrer">{t('Watch GitHub')}</a></button>
     </div>
+
+    
+
+</div>
     </div>
 )
 }
